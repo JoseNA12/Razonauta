0db927f3de74d7d0672553840a239a7ab37ad912
José Navarro <jnava1297@gmail.com>
2019-09-21 17:38:07 -0600

    funciones por hacer

--- src/razonauta.scm
+++ src/razonauta.scm
@@ -1,3 +1,14 @@
 (define (main args)
-  (print '(1 2 3 4))
-  0)
+  (print '(1 2 3))
+  0
+)
+
+(define justifique (lambda () 0))
+
+(define acepte-deduccion (lambda (x) (x)))
+
+(define responda (lambda () 0))
+
+(define prueba-deduccion (lambda () 0))
+
+(define arbol (lambda () 0))
